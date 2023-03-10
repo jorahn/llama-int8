d61498340a4c9118a742fade8d308508634cbab7
Jonathan Rahn <rahn.jonathan@gmail.com>
2023-03-10 21:27:38 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-python example.py --ckpt_dir ~/models/meta--llama/13B/ --tokenizer_path ~/models/meta--llama/tokenizer.model
+python example.py --ckpt_dir ~/models/meta--llama/13B/ --tokenizer_path ~/models/meta--llama/tokenizer.model --max_batch_size=4
